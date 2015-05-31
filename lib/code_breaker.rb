0b1ae98806c738349d1c6414c4acc22b13495ef5
GermanFilipp <ger772@yandex.ru>
2015-05-31 16:01:24 +0300

    Update code_breaker.rb

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -3,5 +3,69 @@ require "code_breaker/game"
 require "code_breaker/gamer"
 require "code_breaker/user"
 module CodeBreaker
- 
+ =begin
+  class Play
+    def initialize
+      @game = Game.new
+      @gamer = Gamer.new
+    end
+
+    def play
+      puts "Play game?(y/n)"
+      result = gets.chomp
+      case result
+        when /y/
+          @game.start
+          true_false = true
+          while true_false
+            puts "Enter number that consist 4 numbers from 1-4.(?-to hints ex - to exit)"
+            answer = gets.chomp
+            case answer
+              when "?"
+                unless @game.available_hints == 0
+                  puts @game.get_hint
+                else
+                 puts "You have not hints"
+                end
+              when /ex/
+                true_false = false
+              else
+                if @game.move == 0
+                  puts "Game over. Secret code is #{@game.secret_code}"
+                  true_false = false
+                else
+                  response = @game.check_number answer
+                  puts response
+                  if response == "++++"
+                    puts "You win!"
+                    puts "Do you want save score?(y/n)"
+                    case gets.chomp
+                      when /y/
+                        puts "Enter your name"
+                        name = gets.chomp
+                        @gamer.load_data
+                        @gamer.add(User.new(name: name, turns: @game.move, complete: 1))
+                        @gamer.save_data
+                        true_false = false
+                      else
+                        true_false = false
+                    end
+                  end
+                end
+            end
+          end
+
+        when /n/
+          puts "ok,Boss :("
+        else
+          puts "something wrong"
+      end
+
+
+    end
+
+  end
+  play = Play.new
+  play.play
+=end
 end
